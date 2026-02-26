72c271dfe2f63140b8217c436e6d48659616f27a
gam <gamakagami4@gmail.com>
2026-02-26 11:57:32 +0700

    remove

--- test_filter.py
+++
@@ -1,9 +0,0 @@
-from server.repositories.mmfltr_repo import fetch_column_comments
-
-# Fetch column comments from barcode.mmfltr
-comments = fetch_column_comments()
-
-print("Field Comments for barcode.mmfltr:\n")
-
-for column, comment in comments.items():
-    print(f"{column} -> {comment}")
